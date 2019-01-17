d3787e90eebccb59f58860b5086986e6b3ce332d
NorthWestCoin <soulwillcrashandburnonice@gmail.com>
2019-01-17 14:52:39 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        public int doMatch(int pervoeChislo, int vtoroeChislo, int tretieChislo){
+            MathFunction operation = new MathFunction();
+            int result1 = operation.multiply(pervoeChislo, vtoroeChislo);
+            int result2 = operation.add(result1, tretieChislo);
+            int result3 = result2 - 1;
+
+            return result3;
+        }
+    }
+}
---
+++ src/com/company/mathFunction.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MathFunction {
+
+    public int multiply(int a, int b){
+        System.out.println(a / b);
+    }
+    public int add(int c, int d){
+        System.out.println(c * d);
+    }
+}
