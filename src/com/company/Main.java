00721fd142c78d03a451d38cd6b04949601afcae
NorthWestCoin <soulwillcrashandburnonice@gmail.com>
2019-01-17 15:10:14 +0200

    оиои

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        public int doMatch(int pervoeChislo, int vtoroeChislo, int tretieChislo){
-            MathFunction operation = new MathFunction();
-            int result1 = operation.multiply(pervoeChislo, vtoroeChislo);
-            int result2 = operation.add(result1, tretieChislo);
-            int result3 = result2 - 1;
 
-            return result3;
-        }
+    }
+
+    public int doMatch(int pervoeChislo, int vtoroeChislo, int tretieChislo){
+        MathFunction operation = new MathFunction();
+        int result1 = operation.multiply(pervoeChislo, vtoroeChislo);
+        int result2 = operation.add(result1, tretieChislo);
+        int result3 = result2 - 1;
+
+        return result3;
     }
 }
